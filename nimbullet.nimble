361f351b3c8f0f78ae727e574c57f69fdcdcb1f5
haxscramper <haxscramper@gmail.com>
2021-02-07 00:15:24 +0300

    [CHECKPOINT]

--- nimbullet.nimble
+++ nimbullet.nimble
@@ -10,6 +10,8 @@ srcDir        = "src"
 # Dependencies
 
 requires "nim >= 1.4.0"
+requires "hcparse >= 0.1.2"
+requires "hmisc >= 0.9.16"
 
 after install:
   exec("nim c -r nimbullet/src/build.nim")
---
+++ src/nimbullet/make_wrap.nim
@@ -0,0 +1,78 @@
+import hcparse, hcparse/[libclang]
+import std/[options]
+import hmisc/other/[oswrap, hshell]
+import hmisc/helpers
+import cxxstd
+
+let paths =
+  evalShell(shCmd(gcc, "-Wp,-v", -x, "c++", "-", "-fsyntax-only")).stderr
+
+let
+  resdir = RelDir("../src/nimtrail/")
+  srcd = AbsDir(currentSourcePath()).splitDir().head
+
+let parseConf = baseCppParseConfig.withIt do:
+  var inList = false
+  for line in split(paths, '\n'):
+
+    if line.endsWith("search starts here:"):
+      inList = true
+      continue
+
+    if "End of search list" in line:
+      inList = false
+
+    if inList:
+      it.includepaths.add AbsDir(realpath line[1..^1])
+
+  echo it.includepaths
+
+
+  it.includepaths.add @[
+    srcd.dir() / "SourcetrailDB/core/include",
+    srcd.dir() / "SourcetrailDB/external/cpp_sqlite/include"
+  ]
+
+  it.globalFlags = @["-xc++"]
+
+
+let wrapConf = baseWrapConf.withDeepIt do:
+  it.parseConf = parseConf
+
+  it.makeHeader = (
+    proc(cursor: CXCursor, conf: WrapConfig): NimHeaderSpec {.closure.} =
+      initHeaderSpec cursor.asGlobalInclude(conf)
+  )
+
+  it.getImport = (
+    proc(dep: AbsFile, conf: WrapConfig): seq[string] {.closure.} =
+      for depConf in @[cxxstd.wrapConf, baseWrapConf]:
+        if depConf.isInLibrary(dep):
+          return depConf.getImport(dep, depConf)
+  )
+
+  it.depResolver = (
+    proc(
+      cursor, referencedBy: CXCursor
+    ): DepResolutionKind {.closure.} =
+      if cursor.isFromMainFile():
+        result = drkWrapDirectly
+
+      else:
+        result = drkImportUses
+  )
+
+
+proc doWrap(infile, outfile: FsFile) =
+  writeWrapped(
+    wrapSingleFile(
+      infile,
+      errorReparseVerbose = false,
+      wrapConf = wrapConf,
+      parseConf = parseConf
+    ),
+    outfile,
+    some AbsDir("/tmp"),
+    @[],
+    wrapConf
+  )
